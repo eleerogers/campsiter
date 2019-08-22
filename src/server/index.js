ca2c03f7864073408b6b2bd2552e7413e4ffd703
Lee Rogers <edwinleerogers@gmail.com>
2019-08-21 21:18:53 -0700

    remove if statement

--- src/server/index.js
+++ src/server/index.js
@@ -14,19 +14,12 @@ const middleware = require('./middleware');
 
 const app = express();
 
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static("client/build"));
-  app.get("/*", function(req, res) {
-    res.sendFile(path.join(__dirname, "./client/build/index.html"));
-  });
-}
 
-else {
-  app.use(express.static(path.join(__dirname, '/client/public')));
-  app.get("/*", function(req, res) {
-    res.sendFile(path.join(__dirname, "./client/public/index.html"));
-  });
-}
+app.use(express.static("client/build"));
+app.get("/*", function(req, res) {
+  res.sendFile(path.join(__dirname, "./client/build/index.html"));
+});
+
 
 app.use(expressSession({
   secret: process.env.EXPRESS_SECRET,
