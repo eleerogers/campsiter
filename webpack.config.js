b95284c783dd1def3af17f09d337f9d86cd2ba82
Lee Rogers <edwinleerogers@gmail.com>
2020-06-06 12:43:06 -0700

    change dotenv in webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
-const Dotenv = require('dotenv-webpack');
+const Dotenv = require('dotenv');
 
 const outputDirectory = 'dist';
 
