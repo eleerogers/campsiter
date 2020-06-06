ae795efb348b524da31779571c4cd33269492b22
Lee Rogers <edwinleerogers@gmail.com>
2020-06-06 13:13:31 -0700

    add webpack const to config file

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const Dotenv = require('dotenv-webpack');
-webpack = require('webpack');
+const webpack = require('webpack');
 
 const outputDirectory = 'dist';
 
