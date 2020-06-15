fed61845d51fa6b5b3729a119513acc0fbe6b526
Lee Rogers <edwinleerogers@gmail.com>
2020-06-15 15:15:07 -0700

    remove footer on landing page

--- src/client/components/footer.js
+++ src/client/components/footer.js
@@ -1,9 +1,16 @@
 import React from 'react';
+import { useHistory } from 'react-router-dom';
 import { Navbar, Container } from 'react-bootstrap';
 
 
 function Footer() {
-  return (
+  const {
+    location: {
+      pathname
+    }
+  } = useHistory();
+
+  return pathname === '/' ? null : (
   <Navbar bg="dark" variant="light" sticky="bottom">
     <Container className="footer-copyright text-center pb-1 pt-1 footer">
       <div className="text-muted font-size-14 ml-auto">
