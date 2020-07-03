3b18ab71cfd5d95e54be2ba55e251b3035cf7bb2
Lee Rogers <edwinleerogers@gmail.com>
2020-07-03 14:54:27 -0700

    clean up commented out code in app.js

--- src/client/App.js
+++ src/client/App.js
@@ -10,73 +10,58 @@ const Landing = importedComponent(
   () => import('./components/landing'),
   { LoadingComponent: Loading }
 );
-// import Landing from './components/landing';
 const CampgroundsHome = importedComponent(
   () => import('./components/campgroundsHome'),
   { LoadingComponent: Loading }
 );
-// import CampgroundsHome from './components/campgroundsHome';
 const NewCampground = importedComponent(
   () => import('./components/newCampground'),
   { LoadingComponent: Loading }
 );
-// import NewCampground from './components/newCampground';
 const CampgroundPage = importedComponent(
   () => import('./components/campgroundPage'),
   { LoadingComponent: Loading }
 );
-// const CampgroundPage = React.lazy(() => import('./components/campgroundPage'));
-// import CampgroundPage from './components/campgroundPage';
 const NewComment = importedComponent(
   () => import('./components/newComment'),
   { LoadingComponent: Loading }
 );
-// import NewComment from './components/newComment';
 const EditComment = importedComponent(
   () => import('./components/editComment'),
   { LoadingComponent: Loading }
 );
-// import EditComment from './components/editComment';
 const Login = importedComponent(
   () => import('./components/login'),
   { LoadingComponent: Loading }
 );
-// import Login from './components/login';
 const Signup = importedComponent(
   () => import('./components/signup'),
   { LoadingComponent: Loading }
 );
-// import Signup from './components/signup';
 const EditCampground = importedComponent(
   () => import('./components/editCampground'),
   { LoadingComponent: Loading }
 );
-// import EditCampground from './components/editCampground';
 const UserProfile = importedComponent(
   () => import('./components/userProfile'),
   { LoadingComponent: Loading }
 );
-// import UserProfile from './components/userProfile';
 const EditUser = importedComponent(
   () => import('./components/editUser'),
   { LoadingComponent: Loading }
 );
-// import EditUser from './components/editUser';
 const Forgot = importedComponent(
   () => import('./components/forgot'),
   { LoadingComponent: Loading }
 );
-// import Forgot from './components/forgot';
 const Contact = importedComponent(
   () => import('./components/contact'),
   { LoadingComponent: Loading }
 );
-// import Contact from './components/contact';
 const Reset = importedComponent(
   () => import('./components/resetPassword'),
   { LoadingComponent: Loading }
 );
-// import Reset from './components/resetPassword';
 import { LoadScriptNext } from '@react-google-maps/api';
 import Layout from './components/layout';
 
