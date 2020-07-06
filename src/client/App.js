8474c62c0411444e3209aaf89c6ce56cf8a2d183
Lee Rogers <edwinleerogers@gmail.com>
2020-07-06 14:54:08 -0700

    try importing lazy and LazyBoundary from react-imported-component

--- src/client/App.js
+++ src/client/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
-import importedComponent from 'react-imported-component';
-import Loading from './components/loading';
+import { lazy, LazyBoundary } from 'react-imported-component';
+// import Loading from './components/loading';
 import 'react-toastify/dist/ReactToastify.css';
 import './bootstrap.colors.css';
 import './app.css';
@@ -8,58 +8,19 @@ import { Switch, Route } from 'react-router-dom';
 import { toast } from 'react-toastify';
 // import lazyComponent from './utils/lazyComponent';
 // const CampgroundsHome = lazyComponent('../components/campgroundsHome');
-const CampgroundsHome = importedComponent(
-  () => import('./components/campgroundsHome'),
-  { LoadingComponent: Loading }
-);
-const NewCampground = importedComponent(
-  () => import('./components/newCampground'),
-  { LoadingComponent: Loading }
-);
-const CampgroundPage = importedComponent(
-  () => import('./components/campgroundPage'),
-  { LoadingComponent: Loading }
-);
-const NewComment = importedComponent(
-  () => import('./components/newComment'),
-  { LoadingComponent: Loading }
-);
-const EditComment = importedComponent(
-  () => import('./components/editComment'),
-  { LoadingComponent: Loading }
-);
-const Login = importedComponent(
-  () => import('./components/login'),
-  { LoadingComponent: Loading }
-);
-const Signup = importedComponent(
-  () => import('./components/signup'),
-  { LoadingComponent: Loading }
-);
-const EditCampground = importedComponent(
-  () => import('./components/editCampground'),
-  { LoadingComponent: Loading }
-);
-const UserProfile = importedComponent(
-  () => import('./components/userProfile'),
-  { LoadingComponent: Loading }
-);
-const EditUser = importedComponent(
-  () => import('./components/editUser'),
-  { LoadingComponent: Loading }
-);
-const Forgot = importedComponent(
-  () => import('./components/forgot'),
-  { LoadingComponent: Loading }
-);
-const Contact = importedComponent(
-  () => import('./components/contact'),
-  { LoadingComponent: Loading }
-);
-const Reset = importedComponent(
-  () => import('./components/resetPassword'),
-  { LoadingComponent: Loading }
-);
+const CampgroundsHome = lazy(() => import('./components/campgroundsHome'));
+const NewCampground = lazy(() => import('./components/newCampground'));
+const CampgroundPage = lazy(() => import('./components/campgroundPage'));
+const NewComment = lazy(() => import('./components/newComment'));
+const EditComment = lazy(() => import('./components/editComment'));
+const Login = lazy(() => import('./components/login'));
+const Signup = lazy(() => import('./components/signup'));
+const EditCampground = lazy(() => import('./components/editCampground'));
+const UserProfile = lazy(() => import('./components/userProfile'));
+const EditUser = lazy(() => import('./components/editUser'));
+const Forgot = lazy(() => import('./components/forgot'));
+const Contact = lazy(() => import('./components/contact'));
+const Reset = lazy(() => import('./components/resetPassword'));
 import NoMatch from './components/noMatch';
 import { LoadScriptNext } from '@react-google-maps/api';
 import Layout from './components/layout';
@@ -71,94 +32,96 @@ function App() {
   return (
     <div className="app-outer">
       <Layout>
-        <Switch>
-          <Route
-            path="/campgroundsHome"
-            exact
-          >
-            <CampgroundsHome />
-          </Route>
-          <Route
-            path="/newCampground"
-            exact
-          >
-            <NewCampground />
-          </Route>
-          <Route
-            path="/editCampground"
-            exact
-          >
-            <EditCampground />
-          </Route>
-          <Route
-            path="/campgrounds/:id"
-            exact
-          >
-            <LoadScriptNext
-              googleMapsApiKey={process.env.REACT_APP_GOOGLE_API_KEY}
-              loadingElement={<div />}
+        <LazyBoundary fallback={<div />}>
+          <Switch>
+            <Route
+              path="/campgroundsHome"
+              exact
             >
-              <CampgroundPage />
-            </LoadScriptNext>
-          </Route>
-          <Route
-            path="/campgrounds/:id/comments/new"
-            exact
-          >
-            <NewComment />
-          </Route>
-          <Route
-            path="/campgrounds/:id/comments/edit"
-            exact
-          >
-            <EditComment />
-          </Route>
-          <Route
-            path="/login"
-          >
-            <Login />
-          </Route>
-          <Route path="/ycusers/:id">
-            <UserProfile />
-          </Route>
-          <Route
-            path="/signup"
-            exact
-          >
-            <Signup />
-          </Route>
-          <Route
-            path="/editUser"
-            exact
-          >
-            <EditUser />
-          </Route>
-          <Route
-            path="/forgot"
-            exact
-          >
-            <Forgot />
-          </Route>
-          <Route
-            path="/contact"
-            exact
-          >
-            <Contact />
-          </Route>
-          <Route
-            path="/contactAdmin"
-            exact
-          >
-            <Contact />
-          </Route>
-          <Route
-            path="/reset/:reset_password_token"
-            exact
-          >
-            <Reset />
-          </Route>
-          <Route component={NoMatch} />
-        </Switch>
+              <CampgroundsHome />
+            </Route>
+            <Route
+              path="/newCampground"
+              exact
+            >
+              <NewCampground />
+            </Route>
+            <Route
+              path="/editCampground"
+              exact
+            >
+              <EditCampground />
+            </Route>
+            <Route
+              path="/campgrounds/:id"
+              exact
+            >
+              <LoadScriptNext
+                googleMapsApiKey={process.env.REACT_APP_GOOGLE_API_KEY}
+                loadingElement={<div />}
+              >
+                <CampgroundPage />
+              </LoadScriptNext>
+            </Route>
+            <Route
+              path="/campgrounds/:id/comments/new"
+              exact
+            >
+              <NewComment />
+            </Route>
+            <Route
+              path="/campgrounds/:id/comments/edit"
+              exact
+            >
+              <EditComment />
+            </Route>
+            <Route
+              path="/login"
+            >
+              <Login />
+            </Route>
+            <Route path="/ycusers/:id">
+              <UserProfile />
+            </Route>
+            <Route
+              path="/signup"
+              exact
+            >
+              <Signup />
+            </Route>
+            <Route
+              path="/editUser"
+              exact
+            >
+              <EditUser />
+            </Route>
+            <Route
+              path="/forgot"
+              exact
+            >
+              <Forgot />
+            </Route>
+            <Route
+              path="/contact"
+              exact
+            >
+              <Contact />
+            </Route>
+            <Route
+              path="/contactAdmin"
+              exact
+            >
+              <Contact />
+            </Route>
+            <Route
+              path="/reset/:reset_password_token"
+              exact
+            >
+              <Reset />
+            </Route>
+            <Route component={NoMatch} />
+          </Switch>
+        </LazyBoundary>
       </Layout>
     </div>
   );
