431c78587b72ac6bbd886f667401ac19a10c84ef
Lee Rogers <edwinleerogers@gmail.com>
2020-07-30 10:38:55 -0700

    alter webpack to only look for .env in dev mode

--- webpack.config.js
+++ webpack.config.js
@@ -81,7 +81,7 @@ module.exports = {
     new HtmlWebpackPlugin({
       template: './public/index.html',
     }),
-    new Dotenv({ path: './.env' }),
+    process.env.NODE_ENV === 'dev' && new Dotenv({ path: './.env' }),
     new webpack.DefinePlugin({
       'process.env': {
         'REACT_APP_GOOGLE_API_KEY': JSON.stringify(process.env.REACT_APP_GOOGLE_API_KEY),
