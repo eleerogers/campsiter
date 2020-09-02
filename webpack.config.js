d24c9e773c0ca6f9e7ea8e8f2a66c9b2fa2a9342
Lee Rogers <edwinleerogers@gmail.com>
2020-09-02 15:08:35 -0700

    get jest testing working with preact

--- webpack.config.js
+++ webpack.config.js
@@ -21,7 +21,7 @@ module.exports = {
   },
   resolve: {
     alias: {
-      "react": "preact/compat",
+      react: "preact/compat",
       "react-dom": "preact/compat",
       "react-dom/test-utils": "preact/test-utils"
     }
